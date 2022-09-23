748bcd2a535bace65dd6b919a6f3e6ea35582593
dht <603928588@qq.com>
2022-09-23 18:01:48 +0800

    暂存

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,6 +8,10 @@
 import { getConfig } from '~/env/config'
 
 const config = getConfig()
+
+// localStorage.setItem('test', '11231')
+//
+// console.log(localStorage.getItem('test'))
 </script>
 
 <style scoped lang="scss"></style>
