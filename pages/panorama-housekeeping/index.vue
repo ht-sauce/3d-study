06aab43fc2fa8fbea98e9e6f9f7df6b6a33250c2
dht <603928588@qq.com>
2023-05-26 16:55:09 +0800

    渲染元素位置调整

--- pages/panorama-housekeeping/index.vue
+++ pages/panorama-housekeeping/index.vue
@@ -15,7 +15,7 @@ onMounted(() => {
     const renderer = new THREE.WebGLRenderer()
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
+    canvasDivRef.value?.appendChild(renderer.domElement)
 
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100)
